35fbaef85fe22c415b1c7b6590fb2dd72b43aabc
pleasewhy <1943788269@qq.com>
2019-11-18 21:00:16 +0800

    添加注释

--- src/线性结构/QueueByArray.java
+++ src/线性结构/QueueByArray.java
@@ -2,40 +2,72 @@ package 线性结构;
 
 import java.util.Iterator;
 
+/**
+ * 用数组实现的队列
+ * @version 1.0
+ * @time 2019-11-18
+ * @author hy
+ */
 public class QueueByArray<T> implements Iterable<T> {
+    private static final int DEFAULTSIZE = 50000;
     private int length = 0;
-    private int size = 50000;
+    private int size = DEFAULTSIZE;
     private T[] items;
     private int head = 0;
     private int tail = 0;
 
+    /**
+     * 初始化一个默认队列的对象
+     */
     public QueueByArray() {
         items = (T[])(new Object[this.size]);
     }
 
+    /**
+     * 初始化一个指定长度队列
+     * @param size 一个队列的最大长度
+     */
     public QueueByArray(int size) {
         this.size = size;
         items = (T[])(new Object[size]);
     }
 
+    /**
+     * 初始化指定初值的队列
+     * @param itemArray 一个数组
+     */
     public QueueByArray(T[] itemArray) {
         items = (T[])(new Object[size]);
         pushAll(itemArray);
     }
 
+    /**
+     * 队列的中包含元素的长度
+     * @return 长度
+     */
     public int length() {
         return length;
     }
 
+    /**
+     * 队列是否为空
+     * @return
+     */
     public boolean empty() {
         return this.length == 0;
     }
 
+    /**
+     * 判断队列是否满元素
+     * @return
+     */
     public boolean fill() {
         return length == size;
     }
 
-    // 将队列向前移动到head为0
+    /**
+     * 移动队列为将队列的head移到0
+     */
     private void move() {
         int tmpHead = 0;
         for (int i = head; i < tail; ++i) {
@@ -45,6 +77,10 @@ public class QueueByArray<T> implements Iterable<T> {
         tail = length;
     }
 
+    /**
+     * 将item送入队列
+     * @param item
+     */
     public void push(T item) {
         if (tail != size) {
             items[tail++] = item;
@@ -58,6 +94,10 @@ public class QueueByArray<T> implements Iterable<T> {
         }
     }
 
+    /**
+     * 将itemArray送入队列
+     * @param itemArray
+     */
     public void pushAll(T[] itemArray) {
         if (size - length < itemArray.length) {
             throw new IndexOutOfBoundsException();
@@ -67,12 +107,20 @@ public class QueueByArray<T> implements Iterable<T> {
         }
     }
 
+    /**
+     * 得到队列的头元素
+     * @return
+     */
     public T get() {
         if (!empty())
             return items[head];
         throw new IndexOutOfBoundsException();
     }
 
+    /**
+     * 取出队列头元素
+     * @return
+     */
     public T pop() {
         T rev = get();
         head += 1;
@@ -80,6 +128,10 @@ public class QueueByArray<T> implements Iterable<T> {
         return rev;
     }
 
+    /**
+     * 用于迭代器，遍历队列的全部元素，不删除
+     * @return 队列迭代器
+     */
     @Override
     public Iterator iterator() {
         return new QueueIterator();
