e350b076900fc62ea7552a026583fac14d013ad2
pleasewhy <1943788269@qq.com>
2019-11-18 15:58:47 +0800

    1.1

--- src/线性结构/LinkedList.java
+++ src/线性结构/LinkedList.java
@@ -3,11 +3,11 @@ package 线性结构;
 import java.util.Iterator;
 
 
-public class LinkedList<T> implements Iterable<T> {
-    int length;  // 用于记录链表的长度
+public class LinkedList<T> implements Iterable {
+    private int length = 0;  // 用于记录链表的长度
     private int MAXlength = 10000;  // 链表的最大长度
-    Node<T> root = new Node<T>();  // 链表的头节点
-    Node<T> tail = new Node<T>();
+    private Node<T> root = new Node<T>();  // 链表的头节点
+    private Node<T> tail = new Node<T>();
 
     public LinkedList() {
         tail.setPrevious(root);
@@ -22,20 +22,25 @@ public class LinkedList<T> implements Iterable<T> {
         return MAXlength;
     }
 
+    public int getLength() {
+        return length;
+    }
+
     public void setMAXlength(int MAXlength) {
         this.MAXlength = MAXlength;
     }
 
     // iterator接口需要实现的方法
+    @Override
     public Iterator<T> iterator() {
         return new LinkedListIterator();//返回了一个迭代器
     }
-
 
     public void add(T data) {
         Node<T> node = new Node<>(data, this.tail.getPrevious());
         this.tail.getPrevious().setNext(node);
         tail.setPrevious(node);
+        this.length += 1;
     }
 
     public void addAll(LinkedList<T> linkedList) {
@@ -62,6 +67,63 @@ public class LinkedList<T> implements Iterable<T> {
         this.length += array.length;
     }
 
+    public void set(int index, T data) {
+        Node<T> node;
+        for (LinkedListIterator itor = new LinkedListIterator(); itor.hasNext(); ) {
+            node = itor.nextNode();
+            if (index == 0) {
+                node.setData(data);
+                return;
+            }
+            index -= 1;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public T get(int index) {
+        Node<T> node;
+        for (LinkedListIterator itor = new LinkedListIterator(); itor.hasNext(); ) {
+            node = itor.nextNode();
+            if (index == 0) {
+
+                return node.getData();
+            }
+            index -= 1;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void remove(int index) {
+        Node<T> node;
+        for (LinkedListIterator itor = new LinkedListIterator(); itor.hasNext(); ) {
+            node = itor.nextNode();
+            if (index == 0) {
+                node.getPrevious().setNext(node.getNext());
+                node.getNext().setPrevious(node.getPrevious());
+                this.length -= 1;
+                return;
+            }
+            index -= 1;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void clear() {
+        root = new Node<T>();
+        tail = new Node<>();
+        this.length = 0;
+    }
+
+    public LinkedList<T> reverse() {
+        LinkedList<T> rev = new LinkedList<>();
+        Node<T> node;
+        int i = 0;
+        for (LinkedListIterator itor = new LinkedListIterator(); i < length; ++i) {
+            rev.add(itor.previousNode().getData());
+        }
+        return rev;
+    }
+
     @Override
     public String toString() {
         Node tmp = this.root;
@@ -76,66 +138,88 @@ public class LinkedList<T> implements Iterable<T> {
 
     private class LinkedListIterator implements Iterator<T> {
         private Node<T> now = root;  //用于迭代器
+        private Node<T> nowTail = tail;
 
         // Iterator接口中需要实现的方法
+        @Override
         public boolean hasNext() {
             return now.hasNext();
         }
 
         // Iterator接口中需要实现的方法
+        @Override
         public T next() {
             T data = now.getNext().getData();
             now = now.getNext();
             return data;
         }
+
+        public Node<T> previousNode() {
+            Node<T> node = nowTail.getPrevious();
+            nowTail = nowTail.getPrevious();
+            return node;
+        }
+
+        Node<T> nextNode() {
+            Node<T> node = now.getNext();
+            now = now.getNext();
+            return node;
+        }
     }
 
-    private class Node<T> {
-        private T data;
-        private Node<T> next;
-        private Node<T> previous;
+    private class Node<t> {
+        private t data;
+        private Node<t> next;
+        private Node<t> previous;
 
         Node() {
 
         }
 
-        Node(T data, Node<T> previous) {
+        Node(t data, Node<t> previous) {
             this.data = data;
             this.previous = previous;
         }
 
-        void setNext(Node<T> next) {
-            this.next = next;
-        }
 
-        T getData() {
+        t getData() {
             return data;
         }
 
-        Node<T> getNext() {
+        Node<t> getNext() {
             return next;
+        }
+
+        Node<t> getPrevious() {
+            return this.previous;
+        }
+
+        void setData(t data) {
+            this.data = data;
+        }
+
+        void setPrevious(Node<t> previous) {
+            this.previous = previous;
+        }
+
+        void setNext(Node<t> next) {
+            this.next = next;
         }
 
         boolean hasNext() {
             return this.next != null;
         }
 
-        Node<T> getPrevious() {
-            return this.previous;
-        }
 
-        void setPrevious(Node<T> previous) {
-            this.previous = previous;
-        }
     }
 
     public static void main(String[] args) {
-        String[] test1 = {"1", "2"};
+        String[] test1 = {"1", "2", "3"};
         Integer[] test2 = {3, 4};
         LinkedList<String> a = new LinkedList<String>();
         LinkedList<Integer> b = new LinkedList<Integer>();
         a.addAll(test1);
         b.addAll(test2);
-        System.out.print(a.toString());
+        System.out.print(a.reverse().toString());
     }
 }
---
+++ src/线性结构/QueueByArray.java
@@ -0,0 +1,109 @@
+package 线性结构;
+
+import java.util.Iterator;
+
+public class QueueByArray implements Iterable {
+    private int length = 0;
+    private int size = 50000;
+    private int[] items;
+    int head = 0;
+    int tail = 0;
+
+    public QueueByArray() {
+        items = new int[this.size];
+    }
+
+    public QueueByArray(int size) {
+        this.size = size;
+        items = new int[size];
+    }
+
+    public QueueByArray(int[] itemArray) {
+        pushAll(itemArray);
+    }
+
+    public boolean empty() {
+        return this.length == 0;
+    }
+
+    public boolean fill() {
+        return length == size;
+    }
+
+    // 将队列向前移动到head为0
+    private void move() {
+        int tmpHead = 0;
+        for (int i = head; i < tail; ++i) {
+            items[tmpHead++] = items[i];
+        }
+        head = 0;
+        tail = length;
+    }
+
+    public void push(int item) {
+        if (tail != size) {
+            items[tail++] = item;
+            length += 1;
+        } else if (head != 0) {
+            this.move();
+            items[tail++] = item;
+            length += 1;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void pushAll(int[] itemArray) {
+        if (size - length < itemArray.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = 0; i < itemArray.length; ++i) {
+            push(itemArray[i]);
+        }
+    }
+
+    public int get() {
+        if (!empty())
+            return items[head];
+        throw new IndexOutOfBoundsException();
+    }
+
+    public int pop() {
+        int rev = get();
+        head -= 1;
+        length -= 1;
+        return rev;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator<Integer> {
+        private int now = head;
+
+        @Override
+        public boolean hasNext() {
+            return !(now == tail);
+        }
+
+        @Override
+        public Integer next() {
+            if (!empty()) {
+                return items[now++];
+            }
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {1, 2, 3, 4, 5, 6};
+        QueueByArray q = new QueueByArray();
+        q.pushAll(test1);
+        for (Object i : q) {
+            System.out.print(i + " ");
+        }
+    }
+}
