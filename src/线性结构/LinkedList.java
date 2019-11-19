b55a6a03c12b2d119dd59c1accf150fce2a1e23d
pleasewhy <1943788269@qq.com>
2019-11-19 09:53:50 +0800

    添加注释

--- src/线性结构/LinkedList.java
+++ src/线性结构/LinkedList.java
@@ -3,34 +3,42 @@ package 线性结构;
 import java.util.Iterator;
 
 /**
- * 链表
- *
+ * 链表一种较为简单的数据结构，其对于删除和添加元素有着较好的表现，
+ * 但不支持随机访问，在大量修改和访问指定位置的元素，表现很差，比
+ * 数组差了几个量级。
+ * 该链表实现了增删改查4个操作，并且支持迭代器
  * @param <T>
  */
 public class LinkedList<T> implements Iterable {
     private int length = 0;  // 用于记录链表的长度
     protected Item<T> head = new Item<T>();  // 链表的头节点
-    protected Item<T> tail = new Item<T>(head);
+    protected Item<T> tail = new Item<T>();  // 链表的尾节点
 
     public LinkedList() {
-        tail.setPrevious(head);
     }
 
+    /**
+     * 将类型为T的数组转化为链表
+     *
+     * @param items 数组
+     */
     public LinkedList(T[] items) {
+        this();
         addAll(items);
     }
 
+    /**
+     * @return 链表的长度
+     */
     public int getLength() {
         return length;
     }
 
-
-    // iterator接口需要实现的方法
-    @Override
-    public Iterator<T> iterator() {
-        return new LinkedListIterator();//返回了一个迭代器
-    }
-
+    /**
+     * 将一个元素添加到链表中
+     *
+     * @param data
+     */
     public void add(T data) {
         Item<T> item = new Item<>(data);
         if (length == 0) {
@@ -44,18 +52,32 @@ public class LinkedList<T> implements Iterable {
         length += 1;
     }
 
+    /**
+     * 将链表添加到链表中
+     *
+     * @param items
+     */
     public void addAll(LinkedList<T> items) {
         for (Iterator<T> itor = items.iterator(); itor.hasNext(); ) {
             add(itor.next());
         }
     }
 
+    /**
+     * 将类型为T的数组添加到链表中
+     * @param items
+     */
     public void addAll(T[] items) {
         for (int i = 0; i < items.length; ++i) {
             add(items[i]);
         }
     }
 
+    /**
+     * 修改下标index的数据，由于链表不能随机访问，不推荐该方法
+     * @param index  要修改的下标
+     * @param item  修改元素的新内容
+     */
     public void set(int index, T item) {
         Item<T> tmp;
         for (LinkedListIterator itor = new LinkedListIterator(); itor.hasNext(); ) {
@@ -69,6 +91,11 @@ public class LinkedList<T> implements Iterable {
         throw new IndexOutOfBoundsException();
     }
 
+    /**
+     *
+     * @param index
+     * @return 下标为index的值
+     */
     public T get(int index) {
         Item<T> item;
         for (LinkedListIterator itor = new LinkedListIterator(); itor.hasNext(); ) {
@@ -95,7 +122,7 @@ public class LinkedList<T> implements Iterable {
         }
         T rev = head.next.data;
         head.setNext(head.getNext().getNext());
-        length-=1;
+        length -= 1;
         return rev;
     }
 
@@ -112,12 +139,13 @@ public class LinkedList<T> implements Iterable {
         }
         T rev = tail.previous.data;
         tail.setPrevious(tail.getPrevious().getPrevious());
-        length-=1;
-        return  rev;
+        length -= 1;
+        return rev;
     }
 
     /**
      * 删除一个元素
+     *
      * @param index 删除元素的下标
      */
     public void remove(int index) {
@@ -143,16 +171,29 @@ public class LinkedList<T> implements Iterable {
         }
     }
 
+    /**
+     * 清空链表
+     */
     public void clear() {
         head = new Item<T>();
         tail = new Item<>();
         this.length = 0;
     }
 
+    /**
+     * 判断链表是否为空
+     *
+     * @return
+     */
     public boolean empty() {
         return length == 0;
     }
 
+    /**
+     * 将链表反转
+     *
+     * @return 反转后的链表
+     */
     public LinkedList<T> reverse() {
         LinkedList<T> rev = new LinkedList<>();
         Item<T> item;
@@ -161,6 +202,15 @@ public class LinkedList<T> implements Iterable {
             rev.add(itor.previousItem().getData());
         }
         return rev;
+    }
+
+    /**
+     * iterator接口需要实现的方法
+     * @return 返回一个迭代器类
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListIterator();//返回了一个迭代器
     }
 
     @Override
@@ -175,6 +225,9 @@ public class LinkedList<T> implements Iterable {
         return rev.toString();
     }
 
+    /**
+     * 链表的迭代器类需要继承Iterator，并完成hasNext()，next()方法
+     */
     private class LinkedListIterator implements Iterator<T> {
         private Item<T> now = head;  //用于迭代器
         private Item<T> nowTail = tail;
