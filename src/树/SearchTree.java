88969f7ae7f1b15b911d6db1f5ed032f60451f13
pleasewhy <1943788269@qq.com>
2019-11-23 20:00:01 +0800

    二分查找树

--- src/树/SearchTree.java
+++ src/树/SearchTree.java
@@ -1,5 +1,8 @@
 package 树;
 
+import com.sun.source.tree.Tree;
+import org.jetbrains.annotations.Contract;
+
 /**
  * 查找树ADT----二叉查找树
  * 其左节点大于（小于）等于父节点右节点小于（大于）父节点，
@@ -12,6 +15,7 @@ public class SearchTree<T extends Comparable> {
     private int length = 0;
     private TreeNode root;
 
+    @Contract(pure = true)
     public SearchTree() {
     }
 
@@ -20,12 +24,14 @@ public class SearchTree<T extends Comparable> {
     }
 
     public void insert(T element) {
+        length++;
         if (root == null)
             root = new TreeNode(element);
         else
             dfsInsert(root, element);
     }
 
+    //    @Contract(value = "null->")
     private void dfsInsert(TreeNode now, T element) {
         if (now == null) {
             return;
@@ -37,10 +43,95 @@ public class SearchTree<T extends Comparable> {
             if (now.left == null)
                 now.left = new TreeNode(element);
         } else {
-            dfsInsert(now.left, element);
+            dfsInsert(now.right, element);
             if (now.right == null)
                 now.right = new TreeNode(element);
         }
+    }
+
+    public void delete(T element) {
+        if(find(element))
+            length--;
+        root = delete(root, element);
+    }
+
+    /**
+     * 该函数删除时的值是向右边（最小值）修改
+     *
+     * @param now
+     * @param element
+     * @return
+     */
+    private TreeNode delete(TreeNode now, T element) {
+        if (now == null)
+            return null;
+        else if (element.compareTo(now.element) > 0) {
+            now.left = delete(now.left, element);  // 去左边删除，并将左边的删除后的值赋值给当前节点的left
+        } else if (element.compareTo(now.element) < 0) {
+            now.right = delete(now.right, element);
+        } else if (now.right != null && now.left != null) {
+            TreeNode node =  findMin(now.left);
+            now.element = node.element;
+            now.frequency = node.frequency;
+            now.left = delete(now.left, now.element);
+        } else {
+            now = (now.right != null) ? now.right : now.left;
+        }
+        return now;
+    }
+
+    /**
+     * element是否存在
+     *
+     * @param element 查找元素
+     * @return true 存在，不存在false
+     */
+    public boolean find(T element) {
+        return find(root, element) != null;
+    }
+
+    /**
+     * 返回element元素节点
+     *
+     * @param now     当前节点
+     * @param element 查找值
+     * @return TreeNode
+     */
+    private TreeNode find(TreeNode now, T element) {
+        if (now == null) {
+            return null;
+        }
+        if (element.compareTo(now.element) == 0) {
+            return now;
+        } else if (element.compareTo(now.element) > 0) {
+            return find(now.left, element);
+        } else {
+            return find(now.left, element);
+        }
+    }
+
+    public T findMax() {
+        return findMax(root).element;
+    }
+
+    private TreeNode findMax(TreeNode now) {
+        if (now == null)
+            return null;
+        if (now.left == null)
+            return now;
+        return findMax(now.left);
+    }
+
+    public T findMin() {
+        return findMin(root).element;
+    }
+
+    private TreeNode findMin(TreeNode now) {
+        if (now == null)
+            return null;
+        if (now.right == null)
+            return now;
+        return findMin(now.right);
     }
 
     private class TreeNode {
@@ -49,8 +140,21 @@ public class SearchTree<T extends Comparable> {
         TreeNode left;  // 左节点
         TreeNode right;  // 右节点
 
+        @Contract(pure = true)
         TreeNode(T element) {
             this.element = element;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1, 2, 3, 6, 9, 10, 45, 654, 452, 485, 45, 45, 78, 21, 2, 2415, 21, 2, 45};
+        SearchTree<Integer> t = new SearchTree<>();
+        for (int i : test
+        ) {
+            t.insert(i);
+        }
+
+        t.delete(2415);
+        System.out.print(t.findMin()+" "+t.findMax());
     }
 }
